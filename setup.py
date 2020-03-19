3fe500d7eaf2672a456c65c65de1029cafdad0be
Jose Patricio V <josepato@linkaform.com>
2020-03-19 17:12:57 -0600

    requierments

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ import datetime, base64, json, urllib2, \
 from urllib2 import Request, urlopen, HTTPError
 INSTALL_REQUIREMENTS = [
     'datetime',
-    'subprocess',
     'sys',
     'simplejson',
     'hashlib',
