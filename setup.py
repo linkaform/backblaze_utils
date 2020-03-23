d61d656759fd8dd55a421b1eec4f03e0aecef054
Jonathan <jonathan.aam92@gmail.com>
2020-03-23 10:13:10 -0600

    Sin Readme

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-
-import datetime, base64, json, urllib2, \
-       subprocess ,sys, simplejson, hashlib, time
-
-from urllib2 import Request, urlopen, HTTPError
 INSTALL_REQUIREMENTS = [
-    'datetime',
-    'simplejson'
+    'simplejson',
 ]
 
 setup(
@@ -24,7 +15,6 @@ setup(
     include_package_data=True,
     license='BSD License',  # example license
     description='Backblze Utils',
-    long_description=README,
     url='https://app.linkaform.com/',
     author='LikaForm',
     author_email='develop@linkaform.com',
